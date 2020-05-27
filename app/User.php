56d2d5084eb3434999bf13503ddc6b7a797b36a3
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-05-27 16:29:49 +0200

    Ok relazione User e category (one to many)

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\User');
     }
+
+    public function categories()
+    {
+       return $this->hasMany('App\Category');
+    }
 }
