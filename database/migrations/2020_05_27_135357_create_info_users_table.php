3caa3cf6bd335fe5fddcd04ceb8c0a4005226bab
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-05-27 16:02:44 +0200

    Creazione Tabella (info_users)  e Migrazione al db ok

---
+++ database/migrations/2020_05_27_135357_create_info_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info_users', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->text('bio');
+            $table->string('linkedin')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('facebook')->nullable();
+            $table->text('path_photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info_users');
+    }
+}
